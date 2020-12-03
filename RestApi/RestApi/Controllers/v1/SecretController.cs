41841d87e1ed90a0f309eb059726a33b4e438c9b
vazhadval <vazhadvalishvili@gmail.com>
2020-12-03 17:57:40 +0400

    apikey based authentication

---
+++ RestApi/RestApi/Controllers/v1/SecretController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApi.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Controllers.v1
+{
+    [ApiKeyAuth]
+    public class SecretController : Controller
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("super secret endpoint");
+        }
+    }
+}
